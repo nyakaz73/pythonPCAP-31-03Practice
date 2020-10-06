ee9144825f0bfd1b62447ac1e53282823125e5cf
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-10-06 17:47:31 +0200

    Custom Exceptions

--- PyhtonPCAP-31-03/classInstanceVariables.py
+++ PyhtonPCAP-31-03/classInstanceVariables.py
@@ -234,3 +234,51 @@ tracked.turn(False)
 
 
 
+print(' Multiple Inheitence violates : single responsibility principle -> thus is not really encouraged')
+print('Always chose COMPOSITION OVER Multiple Inheritence')
+
+print('######################### try except else finally ##################\n\n')
+
+def reciprocal(n):
+    try:
+        n = 1 / n
+    except ZeroDivisionError:
+        print("Division failed")
+        n = None
+    else:
+        print("Everything went fine")
+    finally:                            #finally is always printed unline else which is exceted if there are no errors
+        print("It's time to say goodbye")
+        return n
+
+print(reciprocal(2))
+print(reciprocal(0))
+
+print("############### Making OWN exceptiona ########################## \n\n")
+class PizzaError(Exception):
+    def __init__(self, pizza, message):
+        Exception.__init__(self, message)
+        self.pizza = pizza
+
+
+class TooMuchCheeseError(PizzaError):
+    def __init__(self, pizza, cheese, message):
+        PizzaError.__init__(self, pizza, message)
+        self.cheese = cheese
+
+
+def makePizza(pizza, cheese):
+	if pizza not in ['margherita', 'capricciosa', 'calzone']:
+		raise PizzaError(pizza, "no such pizza on the menu")
+	if cheese > 100:
+		raise TooMuchCheeseError(pizza, cheese, "too much cheese")
+	print("Pizza ready!")
+
+
+for (pz, ch) in [('calzone', 0), ('margherita', 110), ('mafia', 20)]:
+	try:
+		makePizza(pz, ch)
+	except TooMuchCheeseError as tmce:
+		print(tmce, ':', tmce.cheese)
+	except PizzaError as pe:
+		print(pe, ':', pe.pizza)
