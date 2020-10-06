2376bca62ee8966de85172b4b0c542e30efe5432
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-10-06 14:06:06 +0200

    Composition Class

--- PyhtonPCAP-31-03/classInstanceVariables.py
+++ PyhtonPCAP-31-03/classInstanceVariables.py
@@ -205,5 +205,32 @@ two.doanything() #prints do it from Two  , sincce it is being invoked from Two |
 print(one.name)
 print(two.name)
 
+print('#################### COMPOSITION #####################################\n\n')
+print('This is a techinique of composing an object using a different object')
+import time
+
+class Tracks:
+    def changedirection(self, left, on):
+        print("tracks: ", left, on)
+
+class Wheels:
+    def changedirection(self, left, on):
+        print("wheels: ", left, on)
+
+class Vehicle:
+    def __init__(self, controller):
+        self.controller = controller
+
+    def turn(self, left):
+        self.controller.changedirection(left, True)
+        time.sleep(0.25)
+        self.controller.changedirection(left, False)
+
+wheeled = Vehicle(Wheels())
+tracked = Vehicle(Tracks())
+
+wheeled.turn(True)
+tracked.turn(False)
+
 
 
